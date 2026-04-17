77cb5df41f85925e77996793413850b87056febf
onlyfcmkh7 <onlyfcmkh7@gmail.com>
2026-04-17 14:58:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
+const https = require("https");
+
+const TOKEN = process.env.TELEGRAM_TOKEN;
+
+const sendMessage = (chatId, text) => {
+const url = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
+
+const data = JSON.stringify({
+chat_id: chatId,
+text: text,
+});
+
+const options = {
+method: "POST",
+headers: {
+"Content-Type": "application/json",
+"Content-Length": data.length,
+},
+};
+
+const req = https.request(url, options, (res) => {
+res.on("data", (d) => {
+console.log("Response:", d.toString());
+});
+});
+
+req.on("error", (error) => {
+console.error(error);
+});
+
+req.write(data);
+req.end();
+};
+
 console.log("Bot started");
+
+// тестове повідомлення (вставиш свій chat_id далі)
+setTimeout(() => {
+sendMessage("YOUR_CHAT_ID", "Бот працює 🚀");
+}, 5000);
